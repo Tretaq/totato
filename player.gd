2c26614f60c731e5fd65cd4930f573b3ce7924ce
Treta <jakub.jamrozyy@gmail.com>
2025-10-06 23:30:32 +0200

    secondery comit couse it broke again

--- player.gd
+++ player.gd
@@ -1,11 +1,8 @@
 extends CharacterBody2D
 
-
 func _physics_process(delta):
-	# Movement speed in pixels/sec
-	var speed := 600
-	var direction = Input.get_vector("move_left", "move_right", "move_up", "move_down")
-	velocity = direction * speed
+    var direction = Input.get_vector("move_left", "move_right", "move_up", "move_down")
+    velocity = direction * 600
+    move_and_slide()
 
-	
-	move_and_slide()
+    get_node("HappyBoo").play_walk_animation()
